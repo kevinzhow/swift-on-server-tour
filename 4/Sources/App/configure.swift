32ef2252d80da7c6cc4c57c15854a90d66a642c2
kevinzhow <kevinchou.c@gmail.com>
2023-08-08 11:26:10 +0900

    feat(4)

---
+++ 4/Sources/App/configure.swift
@@ -0,0 +1,18 @@
+import Fluent
+import FluentPostgresDriver
+import Vapor
+
+public func configure(_ app: Application) throws {
+    app.databases.use(.postgres(configuration: SQLPostgresConfiguration(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+        tls: .prefer(try .init(configuration: .clientDefault)))
+    ), as: .psql)
+
+    app.migrations.add([CreatePost()])
+
+    try app.register(collection: PostController())
+}
